2e13a0eef632d4e848501c5f5a137c9647314d2a
Ismael Machado <i@ismaelmachado.com>
2023-12-05 21:15:30 -0300

    Midweek 2024

--- vida-y-ministerio.js
+++ vida-y-ministerio.js
@@ -10,6 +10,10 @@ String.prototype.ord = function() {
 
 function fill(item, selector, content, label) {
 	var field = item.find(selector);
+
+	if (label !== undefined) {
+		field.find(".label").text(label);
+	}
 	
 	if (content == null || content === '') {
 		if (selector === '.counselor_B') {
@@ -57,25 +61,25 @@ function fill(item, selector, content, label) {
 	}
 	
 	field.find(".value").text(content);
-	
-	if (label !== undefined) {
-		field.find(".label").text(label);
-	}
 }
 
-function fillZoom(item, link) {
-	var zoom = item.find(".zoom a");
+function fillLink(item, link) {
+	var element = item.find(".link a");
 	
 	if (link === undefined) {
-		zoom.hide();
+		element.hide();
 		return;
 	}
+
+	if (link.indexOf("stream") > 0) {
+		element.find("img").attr("src", "images/jw-stream.png");
+	}
 	
-	zoom.attr("href", link);
+	element.attr("href", link);
 }
 
 
-function processData(meetingsRequest) {
+function processData(meetingsRequest, meetingsV2Request) {
 	var meetingsData = JSON.parse(meetingsRequest.responseText);
 	var formattedData = {};
 	var header = $(".header");
@@ -94,12 +98,13 @@ function processData(meetingsRequest) {
 			}
 			if (isNaN(entry["E".ord()])) {
 				formattedData[date] = {
-					"special-event": entry["E".ord()]
+					"special-event": entry["E".ord()],
+					"link": entry["BL".ord()],
 				};
 				continue;
 			}
 			formattedData[date] = {
-				"zoom": entry["BL".ord()],
+				"link": entry["BL".ord()],
 				"labels": {
 					"living-part-1": entry["AS".ord()],
 					"living-part-2": entry["AU".ord()],
@@ -137,20 +142,109 @@ function processData(meetingsRequest) {
 					"congregation-study": [ [ entry["AX".ord()], entry["AY".ord()] ] ],
 					"final-prayer": entry["BA".ord()],
 					// ---
-					"video": entry["BB".ord()],
-					"sound": entry["BC".ord()],
+					"multimedia": [ entry["BB".ord()], entry["BC".ord()] ],
 					"hall": entry["BG".ord()],
 					"attendants": [ entry["BH".ord()], entry["BI".ord()] ],
 					"parking": entry["BJ".ord()],
 					"stage": entry["BF".ord()],
 					"microphones": [ [ entry["BD".ord()], entry["BE".ord()] ] ],
-					"cleaning": "Grupos " + entry["BK".ord()]
+					"cleaning": "Grupos " + entry["BK".ord()],
+					// ---
+					"teachers-discussion-1": "",
+					"teachers-discussion-2": "",
+					"student-assignment-1": [], 
+					"student-assignment-2": [], 
+					"student-assignment-3": [], 
+					"student-assignment-4": [], 
 				}
 			};
 		}
 	}
 
-	// console.log(formattedData);
+	var meetingsV2Data = JSON.parse(meetingsV2Request.responseText);
+	for (var i = 2; i < meetingsV2Data.values.length; i++) {
+		var entry = meetingsV2Data.values[i];
+		var day = entry["D".ord()];
+		var splittedDay = day.split("/");
+		var date = new Date("20" + splittedDay[2], splittedDay[1] - 1, splittedDay[0]);
+		if (date >= today && entry["F".ord()] != '') {
+			if (formattedData[date] === undefined) {
+				formattedData[date] = [];
+			}
+			if (isNaN(entry["F".ord()])) {
+				formattedData[date] = {
+					"special-event": entry["F".ord()],
+					"link": entry["BK".ord()],
+				};
+				continue;
+			}
+			formattedData[date] = {
+				"link": entry["BK".ord()],
+				"labels": {
+					"treasures-talk": entry["I".ord()],
+					"teachers-discussion-1": entry["P".ord()],
+					"teachers-discussion-2": entry["R".ord()],
+					"student-assignment-1": entry["T".ord()],
+					"student-assignment-2": entry["Y".ord()],
+					"student-assignment-3": entry["AD".ord()],
+					"student-assignment-4": entry["AI".ord()],
+					"student-talk": entry["AN".ord()],
+				 	"living-part-1": entry["AR".ord()],
+				 	"living-part-2": entry["AT".ord()],
+				},
+				"assignments": {
+					"chairman_A": entry["G".ord()],
+					"counselor_B": entry["H".ord()],
+					"treasures-talk": entry["J".ord()],
+					"pearls": entry["L".ord()],
+					"bible-reading": [ entry["N".ord()], entry["O".ord()] ],
+					// ---
+					"teachers-discussion-1": entry["Q".ord()],
+					"teachers-discussion-2": entry["S".ord()],
+					"student-assignment-1": [ 
+					 	[ entry["U".ord()], entry["V".ord()] ], 
+					 	[ entry["W".ord()], entry["X".ord()] ]
+					], 
+					"student-assignment-2": [ 
+					 	[ entry["Z".ord()], entry["AA".ord()] ], 
+					 	[ entry["AB".ord()], entry["AC".ord()] ]
+					], 
+					"student-assignment-3": [ 
+					 	[ entry["AE".ord()], entry["AF".ord()] ], 
+					 	[ entry["AG".ord()], entry["AH".ord()] ]
+					], 
+					"student-assignment-4": [ 
+					 	[ entry["AJ".ord()], entry["AK".ord()] ], 
+					 	[ entry["AL".ord()], entry["AM".ord()] ]
+					], 
+					"student-talk": [ entry["AO".ord()], entry["AP".ord()] ],
+					"living-part-1": entry["AS".ord()],
+					"living-part-2": entry["AU".ord()],
+					"congregation-study": [ [ entry["AW".ord()], entry["AX".ord()] ] ],
+					"final-prayer": entry["AZ".ord()],
+					// ---
+					"multimedia": [ entry["BA".ord()], entry["BB".ord()] ],
+					"hall": entry["BF".ord()],
+					"attendants": [ entry["BG".ord()], entry["BH".ord()] ],
+					"parking": entry["BI".ord()],
+					"stage": entry["BE".ord()],
+					"microphones": [ [ entry["BC".ord()], entry["BD".ord()] ] ],
+					"cleaning": "Grupos " + entry["BJ".ord()],
+					// --- Legacy
+					"teachers-video": "",
+					"initial-call-1": [], 
+					"initial-call-2": [],
+					"return-visit-1": [],
+					"return-visit-2": [],
+					"bible-study": [],
+				}
+			};
+		}
+	}
+
+
+	console.log(formattedData);
+	
 	var lastMonth = 0;
 
 	for (var day in formattedData) {
@@ -175,7 +269,8 @@ function processData(meetingsRequest) {
 		var row = item.find(".row").clone();
 		item.find(".row").remove();
 		var specialEvent = formattedData[day]["special-event"];
-		
+		fillLink(item, formattedData[day]["link"]);
+
 		if (specialEvent !== undefined) {
 			row = row.filter(".special-event").clone();
 			fill(row, ".section.special-event", specialEvent);
@@ -183,7 +278,6 @@ function processData(meetingsRequest) {
 		} else {
 			var labels = formattedData[day]["labels"];
 			var assignments = formattedData[day]["assignments"];
-			fillZoom(item, formattedData[day]["zoom"]);
 
 			for (const assignment in assignments) {
 				row.find(".section.special-event").hide();
@@ -201,11 +295,17 @@ function processData(meetingsRequest) {
 
 function meetings() {
 	var meetingsUrl = 'https://sheets.googleapis.com/v4/spreadsheets/1XBm2Ywv2CEr7yHTjfbIHT_vNvHtD0pTX0B8BXmuaGF0/values/vida%20y%20ministerio?key=AIzaSyD37ddBLRxw48pq0CLXYd2LIjUrneaKk5s';
-
+	var meetingsV2Url = 'https://sheets.googleapis.com/v4/spreadsheets/1XBm2Ywv2CEr7yHTjfbIHT_vNvHtD0pTX0B8BXmuaGF0/values/vida%20y%20ministerio%202024?key=AIzaSyD37ddBLRxw48pq0CLXYd2LIjUrneaKk5s';
+	
 	var meetingsRequest = new XMLHttpRequest();
 	meetingsRequest.open('GET', meetingsUrl);
 	meetingsRequest.onload = function() {
-		processData(meetingsRequest);
+		var meetingsV2Request = new XMLHttpRequest();
+		meetingsV2Request.open('GET', meetingsV2Url);
+		meetingsV2Request.onload = function() {
+			processData(meetingsRequest, meetingsV2Request);
+		}
+		meetingsV2Request.send();
 	}
 	meetingsRequest.send();
 }
